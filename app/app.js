0126ba6b002d8d114087b7f6472cdbaa9606007e
lintaroken <k@8-3.cc>
2019-05-01 12:53:25 +0900

    Show NODE_ENV to console

--- app/app.js
+++ app/app.js
@@ -81,3 +81,5 @@ if (!window.Intl) {
 if (process.env.NODE_ENV === 'production') {
   require('offline-plugin/runtime').install(); // eslint-disable-line global-require
 }
+// eslint-disable-next-line no-console
+console.log(process.env.NODE_ENV);
